1ca58ac92dda6cdf05ed8db9ebf8ac1c25ba080a
jannick <jannick.parmentier@sfr.fr>
2020-10-30 16:37:15 +0100

    entre db et model

---
+++ App/db/Db.php
@@ -0,0 +1,18 @@
+<?php
+$host = 'localhost';
+$db = 'logitud';
+$user = 'root';
+$pass = '';
+$dsn = "mysql::host$host;dbname=$db";
+$options = [
+    \PDO::ATTR_ERRMODE             => \PDO::ERRMODE_EXCEPTION,
+    \PDO::ATTR_DEFAULT_FETCH_MODE  => \PDO::FETCH_ASSOC,
+    \PDO::ATTR_EMULATE_PREPARES    => false,
+];
+
+try {
+    $pdo = new \PDO($dsn, $user, $pass, $options);
+    echo 'Database connexion established! -';
+} catch (\PDOException $e) {
+    throw new \PDOException($e->getMessage(), $e->getCode());
+}
---
+++ App/models/model.php
@@ -0,0 +1,38 @@
+<?php
+namespace App\models;
+
+require '../db/connectDb.php';
+use \PDO;
+
+class createConnexion {
+    private $name;
+    private $user;
+    private $pass;
+    private $host;
+    private $pdo;
+
+    public function __construct($name, $user = 'root', $pass = '', $host = 'localhost')
+    {
+        $this->name = $name;
+        $this->user = $user;
+        $this->pass = $pass;
+        $this->host = $host;
+    }
+
+    private function getPDO(): PDO
+    {
+        if ($this->pdo === null) {
+            $pdo = new PDO('mysql:dbname=logitud; host=localhost', 'root', '');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
+            $this->pdo = $pdo;
+        }
+        return $this->pdo;
+    }
+
+    public function query($statement)
+    {
+        $req = $this->getPDO()->query($statement);
+        return $req->fetchAll(PDO::FETCH_OBJ);
+    }
+
+}
